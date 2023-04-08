a6dcb975e277a61b6cc583c7c96908e0804eab58
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-07 21:00:56 -0300

    feat: setup pipeline

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Download jq
+curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o ./jq 
+
+# Make jq executable
+chmod a+x ./jq 
+
+# Get version from package.json
+VERSION=$(node --eval="process.stdout.write(require('./package.json').version)")
+
+# Get the current import-map.json
+aws s3 cp s3://mfe-stage-pass/config/import-map.json import-map.json
+
+# Update the import-map.json
+NEW_URL=/config/mfe/util-state/$VERSION/stagepass-util-state.js
+
+# Update the import-map.json
+test -s ./import-map.json && cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/util-state"] = $NEW_URL' > new.importmap.json || echo '{"imports": {"@stagepass/util-state": "'"$NEW_URL"'"}}' > new.importmap.json
+
+# Upload the new import-map.json
+aws s3 cp dist s3://mfe-stage-pass/config/mfe/util-state/$VERSION --recursive
+aws s3 cp new.importmap.json s3://mfe-stage-pass/config/import-map.json
+aws cloudfront create-invalidation --distribution-id E3AG25M6KMU46Y --paths '/config/import-map.json'
