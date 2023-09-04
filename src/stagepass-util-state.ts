2d32903e3b1fd69a7e07dba099a8074d22b3140d
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-09-04 07:48:27 -0300

    feat: create redirect url with multiple mfes

--- src/stagepass-util-state.ts
+++ src/stagepass-util-state.ts
@@ -5,21 +5,25 @@ import { TicketsDTO } from "./models/Ticket";
 type TicketDataInfo = {
   ticketInfo: TicketsDTO | {};
   userInfo: any;
+  redirectUrl: string | null; // Define the new redirectUrl property
 };
 
 type CheckoutTicketData = {
   setTicketInfo: (ticketInfo: any) => void;
   setUserInfo: (userInfo: any) => void;
+  setRedirectUrl: (redirectUrl: string | null) => void; // Add a setter for redirectUrl
 };
 
 const store = combine<TicketDataInfo, CheckoutTicketData>(
   {
     ticketInfo: {},
     userInfo: {},
+    redirectUrl: null,
   },
   (set) => ({
     setTicketInfo: (ticketInfo) => set({ ticketInfo }),
     setUserInfo: (userInfo) => set({ userInfo }),
+    setRedirectUrl: (redirectUrl) => set({ redirectUrl }),
   })
 );
 
